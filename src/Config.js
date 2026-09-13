4c701c7ad054f6b032221dfc95d10d7ea5b056b3
webmasterayso154 <webmasterayso154@gmail.com>
2026-09-13 05:28:13 -0700

    feat: add initial project configuration

---
+++ src/Config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  SEASON: "Fall 2026",
+  REGION_PREFIX: "E154",
+  DROP_FOLDER_ID: "YOUR_FOLDER_ID",
+  ARCHIVE_FOLDER_ID: "YOUR_ARCHIVE_ID",
+  FORM_ID: "YOUR_GOOGLE_FORM_ID",
+  REF_ADMIN_EMAIL: "refadmin@ayso154.org",
+  GMAIL_LABEL: "Ref-Processed",
+  SHEET_TABS: {
+    RAW: "Raw_Import",
+    MASTER_HOME: "Master_Home",
+    REF_DATA: "MatchTrak_Ref_Data",
+    AUDIT: "Ref_Audit",
+    SETUP_TAKEDOWN: "Setup_Takedown",
+    FIELD_GRID: "Field_Grid"
+  }
+};
+
+Object.freeze(CONFIG);
